8f83b8e5e8de19945a62976ddb6ee806e8226dbd
av40798 <amit40798@gmail.com>
2018-01-29 20:50:34 +0530

    Number negative or positive

---
+++ Pos_Neg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	/* code */
+	int number;
+	scanf("%d",&number);
+	if (number >= 0)
+	{
+		/* code */
+		printf("POSITIVE\n");
+	}
+	else
+		printf("NEGATIVE\n");
+
+	return 0;
+	}
+}
